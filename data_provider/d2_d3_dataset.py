44e0fb28ac4a554c538220b45609f2cd61e453ca
sumin124 <79127883+sumin124@users.noreply.github.com>
2024-10-04 14:55:40 +0900

    Update codes

--- data_provider/d2_d3_dataset.py
+++ data_provider/d2_d3_dataset.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 from torch import optim
-#from torch.utils import data
-#from torch.utils.data import DataLoader
 from torch_geometric.data import Batch
 
 from tqdm import tqdm
@@ -31,7 +29,6 @@ from functools import lru_cache
 
 from torch_geometric.data import InMemoryDataset
 
-### For PubChem dataset call (ChEBI also) ###
 
 class PubChemDataset(InMemoryDataset):
     def __init__(self, path):
@@ -69,8 +66,6 @@ class pt_dataset_cid:
 
         if not hasattr(self, 'env'):
             self.connect_db(self.db_path, save_to_self=True)
-        # datapoint_pickled = self.env[env['cid']==cid]
-        # data = self.env[self.env['cid']==cid]
         data = self.env[idx]
         return data  
 
@@ -107,90 +102,6 @@ class pt_dataset_smiles: ## for lm24 dataset (NO cid numbers)
         data = self.env[idx]
         return data  
         
-# class pt_dataset(Dataset):
-
-#     def __init__(self, path, dictionary, max_atoms=256, enriched_description=False):
-
-
-#         self.dictionary = dictionary
-#         self.num_types = len(dictionary)
-#         self.bos = dictionary.bos()
-#         self.eos = dictionary.eos()
-
-#         self.pk_3d_dataset = pt_dataset_cid(path)
-
-#         self.max_atoms = max_atoms
-#         self.remove_hydrogen = True
-#         self.remove_polar_hydrogen = False
-#         self.normalize_coords = True
-#         self.add_special_token = True
-#         self.__max_atoms = 512
-
-#         self.enriched_description = enriched_description
-
-#     def __len__(self):
-        
-#         return len(self.pk_3d_dataset)
-
-#     def __getitem__(self,idx):
-        
-#         '''
-#         data[0] : cid
-#         data[1] : text
-#         data[2] : smiles
-#         data[3] : atom_vec
-#         data[4] : dist
-#         data[5] : edge type
-#         data[6] : coordinate
-#         data[7] : d2_graphs
-#         data[8] : enriched description
-#         data[9] : Molca description (without explicit molecule name)
-#         '''
-
-#         data = self.pk_3d_dataset[idx]
-#         smiles = data[2]
-
-#         if self.enriched_description == True: ##############################
-#             description = data[8]
-#         else:
-#             description = data[1]
-#         ## deal with 3d coordinates
-#         # atoms_orig = np.array(data[3]) ######### 기존 : atom 원자 불러와서 원자 번호로 변환 -> 처음부터 원자 번호 (atom vec)
-#         # atoms = atoms_orig.copy()
-#         atom_vec = np.array(data[3])
-        
-#         # coordinate_set = data[6]
-#         # coordinates = random.sample(coordinate_set, 1)[0].astype(np.float32)
-#         coordinates = data[6]
-        
-#         assert len(atom_vec) == len(coordinates) and len(atom_vec) > 0
-#         assert coordinates.shape[1] == 3
-
-#         ## deal with cropping
-#         if len(atom_vec) > self.max_atoms:
-#             index = np.random.permutation(len(atom_vec))[:self.max_atoms]
-#             atom_vec = atom_vec[index]
-#             coordinates = coordinates[index]
-            
-#         assert 0 < len(atom_vec) <= self.__max_atoms
-
-#         atom_vec = torch.from_numpy(atom_vec).long()
-
-#         if self.normalize_coords:
-#             coordinates = coordinates - coordinates.mean(axis=0)
-
-#         if self.add_special_token:
-#             atom_vec = torch.cat([torch.LongTensor([self.bos]), atom_vec, torch.LongTensor([self.eos])])
-#             # coordinates = np.concatenate([np.zeros((1, 3)), coordinates, np.zeros((1, 3))], axis=0)
-#             coordinates = np.concatenate([np.zeros((1, 3)), coordinates.float() , np.zeros((1, 3))], axis=0)
-
-#         ## obtain edge types; which is defined as the combination of two atom types
-#         edge_type = atom_vec.view(-1, 1) * self.num_types + atom_vec.view(1, -1)
-#         dist = distance_matrix(coordinates, coordinates).astype(np.float32)
-#         coordinates, dist = torch.from_numpy(coordinates), torch.from_numpy(dist)
-#         # return atom_vec, coordinates, edge_type, dist, smiles, description, enriched_description
-#         return atom_vec, coordinates, edge_type, dist, smiles, description, data[7]
-
 
 class pt_dataset(Dataset):
 
@@ -282,77 +193,6 @@ class pt_dataset(Dataset):
         # return atom_vec, coordinates, edge_type, dist, smiles, description, enriched_description
         return atom_vec, coordinates, edge_type, dist, smiles, description, data[7]
         
-# class pt_MolDataset(Dataset):
-
-#     def __init__(self, root, tokenizer, text_max_len, unimol_dict=None, max_atoms=256, prompt='', return_prompt=False, enriched_description=False):
-
-#         super(pt_MolDataset, self).__init__()
-#         self.prompt = prompt
-#         self.return_prompt = return_prompt
-#         self.tokenizer = tokenizer
-        
-#         self.enriched_description = enriched_description #########
-
-#         self.root = root
-#         self.text_max_len = text_max_len
-        
-#         target_path = root
-#         # target_path = os.path.join(root, '3d-dataset_toy.pt')
-#         # target_path = os.path.join(root, '3d-dataset.pt')
-#         if 'PubChem' in root: ##########
-#             target_path = os.path.join(root, 'pubchem_2d_3d.pt') ##########
-            
-#         print(f'Read from {target_path}') ##########
-        
-
-#         self.pt_dataset = pt_dataset(target_path, unimol_dict, max_atoms, enriched_description=self.enriched_description)
-# #        self.cid_list = self.pt_dataset.pk_3d_dataset.cids
-#         self.permutation = None
-
-#     def shuffle(self):
-
-#         ## shuffle the dataset using a permutation matrix
-#         self.permutation = torch.randperm(len(self)).numpy()
-#         return self
-
-#     def __len__(self):
-#         return len(self.pt_dataset)
-
-#     def get_2d_3d(self, index):
-#         atom_vec, coordinates, edge_type, dist, smiles, description, d2_graph = self.pt_dataset[index]
-        
-
-#         if self.return_prompt:
-
-#             smiles_prompt = self.prompt.format(smiles[:96]) #### 96??
-#             return (atom_vec, coordinates, edge_type, dist, smiles), d2_graph, smiles_prompt, description, index
-
-# #        return (atom_vec, coordinates, edge_type, dist, smiles), d2_graph, self.tokenizer_text(description)
-#         return (atom_vec, coordinates, edge_type, dist, smiles), d2_graph, description
-
-
-#     def __getitem__(self, index):
-
-#         ## consider the permutation
-#         if self.permutation is not None:
-#             index = self.permutation[index]
-
-#         return self.get_2d_3d(index)
-
-#     def tokenizer_text(self, text):
-#         sentence_token = self.tokenizer(text=text,
-#                                         truncation=True,
-#                                         padding='max_length',
-#                                         add_special_tokens=True,
-#                                         max_length=self.text_max_len,
-#                                         return_tensors='pt',
-#                                         return_attention_mask=True)
-#         input_ids = sentence_token['input_ids']
-#         attention_mask = sentence_token['attention_mask']
-#         return input_ids, attention_mask
-
-
-
 class pt_MolDataset(Dataset):
 
     def __init__(self, root, tokenizer, text_max_len, unimol_dict=None, max_atoms=256, prompt='', return_prompt=False, enriched_description=False, lm24=False):
@@ -430,14 +270,9 @@ class pubchem_to_2d_3d(Dataset):
 
     def __init__(self, pt_dataset, d3_dataset):
         
-        # self.cids_init = pickle_df['cid'].to_list()
         self.cids_init = pt_dataset.cid
-        # self.texts_init = pickle_df['text'].to_list()
         self.texts_init = pt_dataset.text
-        # self.smiles_init = pickle_df['SMILES'].to_list()
         self.smiles_init = pt_dataset.smiles
-
-        # self.d2_graphs_init = datasets
 
         
 
@@ -449,24 +284,16 @@ class pubchem_to_2d_3d(Dataset):
         append_index = 0
         
         for cid in tqdm(self.cids_init):
-        # for cid in tqdm(self.cids_init)[:100]:
 
             try:
             
-                # atom_vec, dist, edge_type, coordinate = self.get_graph(smile)
                 atom_vec, coordinate, edge_type, dist, smile, description, enriched_description = d3_dataset[cid]
-                # atom_vec, dist, edge_type, coordinate, description, enriched_description = d3_dataset[cid][0], d3_dataset[cid][3], d3_dataset[cid][2], d3_dataset[cid][1], d3_dataset[cid][5], d3_dataset[cid][6]
-                # atom_vec, dist, edge_type, coordinate = atom_vec.unsqueeze(0), dist.unsqueeze(0).to(tensor_type), edge_type.unsqueeze(0), coordinate.unsqueeze(0).to(tensor_type)
-                #atom_vec, dist, edge_type, coordinate = atom_vec, dist.to(tensor_type), edge_type, coordinate.to(tensor_type)
                 self.atom_vecs.append(atom_vec)
                 self.dists.append(dist)
                 self.edge_types.append(edge_type)
                 self.coordinates.append(coordinate)
                 self.texts.append(description) ### with molecular name ([Molecule name] is...)
                 self.enriched_texts.append(enriched_description)
-                
-                # mol = Chem.MolFromSmiles(smile)
-                # self.d2_graphs.append(mol_to_graph_data_obj_simple(mol))
     
                 self.d2_graphs.append(Data(pt_dataset[append_index].x, pt_dataset[append_index].edge_index, pt_dataset[append_index].edge_attr))
                 
@@ -513,11 +340,6 @@ class pubchem_to_2d_3d(Dataset):
         ## normalize coordinates:
         coordinates = coordinates - coordinates.mean(axis=0)
 
-        # ## add_special_token:
-        # atom_vec = torch.cat([torch.LongTensor([dictionary.bos()]), atom_vec, torch.LongTensor([dictionary.eos()])])
-        # coordinates = np.concatenate([np.zeros((1, 3)), coordinates, np.zeros((1, 3))], axis=0)
-        
-        ## obtain edge types; which is defined as the combination of two atom types
         edge_type = atom_vec.view(-1, 1) * len(dictionary) + atom_vec.view(1, -1)
         dist = distance_matrix(coordinates, coordinates).astype(np.float32)
         coordinates, dist = torch.from_numpy(coordinates), torch.from_numpy(dist)
@@ -604,8 +426,6 @@ class pickle_to_2d_3d(Dataset):
         for smile in tqdm(self.smiles_init):
             try:
                 atom_vec, dist, edge_type, coordinate = self.get_graph(smile)
-                # atom_vec, dist, edge_type, coordinate = atom_vec.unsqueeze(0), dist.unsqueeze(0).to(tensor_type), edge_type.unsqueeze(0), coordinate.unsqueeze(0).to(tensor_type)
-                #atom_vec, dist, edge_type, coordinate = atom_vec, dist.to(tensor_type), edge_type, coordinate.to(tensor_type)
                 self.atom_vecs.append(atom_vec)
                 self.dists.append(dist)
                 self.edge_types.append(edge_type)
@@ -656,10 +476,6 @@ class pickle_to_2d_3d(Dataset):
         ## normalize coordinates:
         coordinates = coordinates - coordinates.mean(axis=0)
 
-        # ## add_special_token:
-        # atom_vec = torch.cat([torch.LongTensor([dictionary.bos()]), atom_vec, torch.LongTensor([dictionary.eos()])])
-        # coordinates = np.concatenate([np.zeros((1, 3)), coordinates, np.zeros((1, 3))], axis=0)
-        
         ## obtain edge types; which is defined as the combination of two atom types
         edge_type = atom_vec.view(-1, 1) * len(dictionary) + atom_vec.view(1, -1)
         dist = distance_matrix(coordinates, coordinates).astype(np.float32)
@@ -715,8 +531,7 @@ class MyCollater:
         padded_dist = data_utils.collate_tokens_2d(dist, 0, left_pad=False, pad_to_multiple=self.pad_to_multiple) # shape = [batch_size, max_atoms, max_atoms]
         # print(text_batch)
         text_tokens = self.tokenizer(text_batch,
-                                     truncation=True, ###################################################
-                                     # truncation='only_second', ###################################################
+                                     truncation=True, 
                                      padding='max_length',
                                      add_special_tokens=True,
                                      max_length=self.text_max_len,
@@ -724,8 +539,6 @@ class MyCollater:
                                      return_attention_mask=True, 
                                      return_token_type_ids=False)
         
-        # d2_batch = Batch.from_data_list(d2_batch)
-        # print(d2_batch)
         d2_batch = self.d2_graph_encoder_batch(*d2_batch) ###########
 
         return (padded_atom_vec, padded_dist, padded_edge_type), text_tokens, d2_batch
